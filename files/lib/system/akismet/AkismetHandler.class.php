fdfa1539abc192cedaef3f78bb32b91b36e2bd86
Markus Bartz <roul@codingcorner.info>
2013-05-18 10:35:39 +0200

    Added skeleton

---
+++ files/lib/system/akismet/AkismetHandler.class.php
@@ -0,0 +1,18 @@
+<?php
+namespace wcf\system\akismet;
+
+use wcf\system\exception\UserInputException;
+use wcf\util\HTTPRequest;
+
+/**
+ * Handles Akismet support.
+ * 
+ * @author		Markus Bartz <roul@codingcorner.info>
+ * @copyright	2013 Markus Bartz
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package		info.codingcorner.wcf.akismet
+ * @subpackage	system.akismet
+ * @category	Community Framework
+ */
+class AkismetHandler extends SingletonFactory {
+}
